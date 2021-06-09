1a58e49d71bacd9905650c66b1e0ecb10f73c162
unknown <luhong@miniwan.com>
2021-06-09 16:33:45 +0800

    add: 添加right-menu 组件

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -6,6 +6,32 @@
       @toggleClick="toggleSideBar"
     ></hamburger>
     <breadcrumb class="breadcrumb-container" />
+    <div class="right-menu">
+      <el-dropdown trigger="click" class="avatar-container">
+        <div class="avatar-wrapper">
+          <img
+            src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80"
+            class="user-avatar"
+          />
+          <i class="el-icon-caret-bottom"></i>
+        </div>
+        <el-dropdown-menu slot="dropdown" class="user-dropdown">
+          <el-dropdown-item>
+            <router-link to="/">Home</router-link>
+          </el-dropdown-item>
+          <el-dropdown-item>
+            <a
+              href="https://github.com/LuHong-HL/vue2-element-admin"
+              target="_blank"
+              >Github</a
+            >
+          </el-dropdown-item>
+          <el-dropdown-item divided @click.native="logout">
+            <span>Log Out</span>
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
   </div>
 </template>
 
@@ -27,6 +53,9 @@ export default {
   methods: {
     toggleSideBar() {
       this.isActive = !this.isActive
+    },
+    logout() {
+      console.log('退出登录')
     }
   }
 }
@@ -52,5 +81,31 @@ export default {
 }
 .breadcrumb-container {
   float: left;
+}
+.right-menu {
+  float: right;
+  height: 100%;
+  line-height: 50px;
+
+  .avatar-container {
+    margin-right: 30px;
+    .avatar-wrapper {
+      margin-top: 5px;
+      position: relative;
+      .user-avatar {
+        cursor: pointer;
+        width: 40px;
+        height: 40px;
+        border-radius: 10px;
+      }
+      .el-icon-caret-bottom {
+        cursor: pointer;
+        position: absolute;
+        right: -20px;
+        top: 25px;
+        font-size: 12px;
+      }
+    }
+  }
 }
 </style>
